2aea6af8a1a72d9e0c6f05b67bdc7941e649451b
KustoSan <kustosan@gmail.com>
2015-09-29 02:23:08 +0200

    add followers

---
+++ web/followers.php
@@ -0,0 +1,10 @@
+<?php
+
+header('Access-Control-Allow-Origin: http://kustosan.github.io');
+header('Access-Control-Allow-Origin: https://kustosan.github.io');
+
+$user = $_GET['user'];
+$page = $_GET['page'];
+$rest_json = file_get_contents("https://hummingbird.me/users?followers_of=$user&page=$page"); echo $rest_json;
+
+?>
