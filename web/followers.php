4480ed60c0c569ef6f038fceba9c307ad175c848
KustoSan <kustosan@gmail.com>
2015-09-29 16:12:42 +0200

    header back to php --> *

--- web/followers.php
+++ web/followers.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
 $user = $_GET['user'];
 $page = $_GET['page'];
 $rest_json = file_get_contents("https://hummingbird.me/users?followers_of=$user&page=$page"); echo $rest_json;
--- web/hblibrary.php
+++ web/hblibrary.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
 $user = $_GET['user'];
 $status = $_GET['status'];
 $rest_json = file_get_contents("https://hummingbird.me/api/v1/users/$user/library?status=$status"); echo $rest_json;
